ac24e1c44c0f1f96e09f16babe9917eb16d2c231
Varun Raj <varun@skcript.com>
2023-07-10 08:39:41 +0530

    feat: added analytics
    Signed-off-by: Varun Raj <varun@skcript.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,13 @@ import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
 
+import Analytics from '../components/shared/Analytics';
+
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 );
 
 export default MyApp;
